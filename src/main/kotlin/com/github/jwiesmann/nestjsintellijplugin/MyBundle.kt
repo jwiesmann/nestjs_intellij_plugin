c9e4b9966f6b6bcb9305a78d3827b486821fc878
GitHub Action <action@github.com>
2020-12-05 12:35:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/jwiesmann/nestjsintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/jwiesmann/nestjsintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jwiesmann.nestjsintellijplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/jwiesmann/nestjsintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jwiesmann.nestjsintellijplugin.services
+
+import com.github.jwiesmann.nestjsintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
